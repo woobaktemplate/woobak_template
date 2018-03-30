57b81ae4b6944a52cdea2decfabe9ecc3ec54692
artlyhub <ppark9553@gmail.com>
2018-03-30 18:40:54 +0900

    changing example topic max length

--- examples/models.py
+++ examples/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Template(models.Model):
     category = models.CharField(max_length=50)
-    topic = models.CharField(max_length=50)
+    topic = models.CharField(max_length=200)
     template = models.TextField()
     translation = models.TextField()
 
